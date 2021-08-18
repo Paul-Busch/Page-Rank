b6babea95026aaffa467f837608df3d5b42d9f45
Paul Busch <paul_busch@gmx.de>
2021-08-18 19:34:47 +0200

    added test case in class Table()

--- table.py
+++ table.py
@@ -1,26 +1,31 @@
-lst1 = ["2","3","4","5","6"]
-lst2 = ["3","4","5"]
-lst3 = ["1", "6"]
-lst4 = ["1", "3"]
-lst5 = ["1", "6"]
-lst6 = ["2", "4"]
+
 
 class MyHtmlParser():
-    def __init__(self): pass
 
-    def handleStartTag(html_link):
-        if html_link == "1":
-            return lst1
-        elif html_link == "2":
-            return lst2
-        elif html_link == "3":
-            return lst3
-        elif html_link == "4":
-            return lst4
-        elif html_link == "5":
-            return lst5
-        elif html_link == "6":
-            return lst6
+    lst1 = ["2","3","4","5","6"]
+    lst2 = ["3","4","5"]
+    lst3 = ["1", "6"]
+    lst4 = ["1", "3"]
+    lst5 = ["1", "6"]
+    lst6 = ["2", "4"]
+
+    def __init__(self, html_link): 
+        self.html_link = html_link 
+        
+
+    def handleStartTag(self):
+        if self.html_link == "1":
+            return MyHtmlParser.lst1
+        elif self.html_link == "2":
+            return MyHtmlParser.lst2
+        elif self.html_link == "3":
+            return MyHtmlParser.lst3
+        elif self.html_link == "4":
+            return MyHtmlParser.lst4
+        elif self.html_link == "5":
+            return MyHtmlParser.lst5
+        elif self.html_link == "6":
+            return MyHtmlParser.lst6
 
 class Table(): 
 
@@ -41,16 +46,17 @@ class Table():
         init_table = dict()
         starting_point = self.html_link
 
-        for link in MyHtmlParser.handleStartTag(starting_point):     
+        for link in MyHtmlParser(starting_point).handleStartTag():     
         # hier werden doppelte keys überschrieben
            #create initial table where keys are from the list of the given html_link
-            init_table[link] = MyHtmlParser.handleStartTag(link)
+            init_table[link] = MyHtmlParser(link).handleStartTag()
         
-        print(table)
+        #print(init_table)
         return init_table
     
     def update_table(self):
-        pass
+        table = self.create_init_table()
+        print(table)
    
 
 
@@ -58,6 +64,6 @@ class Table():
 
 
         
-
-table1 = Table("1").get_table()
+t = Table("1")
+table1 = t.update_table()
 
