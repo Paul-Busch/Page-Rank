1ac208b072fc7c46aa77667dc25ddbfdd512e86a
Paul Busch <paul_busch@gmx.de>
2021-09-27 21:45:59 +0200

    Montag Abend

--- main_m.py
+++ main_m.py
@@ -1,15 +1,17 @@
-import merging_file as mf
-import concurrent.futures
+#import merging_file as mf
+#import concurrent.futures
+import search_engine as se
 
 if __name__ == "__main__":
-    t1 = mf.Table("https://www.math.kit.edu")
+    """t1 = mf.Table("https://www.math.kit.edu")
     table = t1.get_table()
     matr = mf.EvalMatrix(table)
-    s = matr.sort_links()
+    s = matr.sort_links()"""
 
-    word = input("Search for: ")
-    for link in s.keys():
-        if word in link.text:
-            print(link)
+    word = str(input("Search for: "))
+    final_search = se.SearchEngine(word)
+    final_search.print_output()
+
+    
             
 
--- search_engine.py
+++ search_engine.py
@@ -5,7 +5,7 @@ class SearchEngine():
     
     def __init__(self, word):
         self.word = word
-        self.dictionary_final = {}
+        # self.dictionary_final = {}
 
     def get_document(self, link):
         '''
@@ -15,8 +15,8 @@ class SearchEngine():
         modified_link = str(link)
         for char in "/\:#.":
             modified_link = modified_link.replace(char,"")
-        with open (modified_link, "w+" , encoding="utf8") as link_text:
-            return link_text
+        return '/Users/Paul/Documents/Uni/Master 5. Semester/Einführung in Python/Page-Rank/html_data/'+modified_link+'.txt'
+            
     
     def get_dict(self):
         '''
@@ -25,7 +25,7 @@ class SearchEngine():
         '''
         with open('sorted.json') as json_file:
             sorted_dict = json.load(json_file)
-            print(sorted_dict)
+            #print(sorted_dict)
         return sorted_dict
     
     def print_output(self):
@@ -33,10 +33,11 @@ class SearchEngine():
         # loop über dict_values und schaue ob der jeweilige link keine leere liste in dic_lines aufweist 
         # liste mit relevanten html links -> sortiert 
         # unter jedem link (entweder fett oder eingerückt) die relevanten zeilen ausgeben
-        self.paralleled()
-        for link in self.dictionary_final:
-            #if paralled().value()[1] =! none:
-            print(link, self.dictionary_final[link][1])
+        dictionary_final = self.paralleled()
+
+        for link in dictionary_final:
+            if dictionary_final[link]:
+                print(link,'\n', dictionary_final[link][1])
 
     """def print_output(word, data, link):
         search_for_word(word, data, link)
@@ -55,28 +56,52 @@ class SearchEngine():
             for line in lines:
                 if self.word in line:
                     lst_of_lines.append(line)
+        #print(lst_of_lines)
         return lst_of_lines
 
-    #TODO Sontraud: Funktion testen --> d2 ist instanz aus anderer Klasse
-    def get_dict_of_lines(self, link, d2):
+    #TODO Sontraud: Funktion testen --> dict_full_info ist instanz aus anderer Klasse
+    def get_dict_of_lines(self, link, dict_full_info):
         doc = self.get_document(link)
+        #print(doc)
         list_of_lines = self.get_list_of_lines(doc)
+        #print(list_of_lines)
         if list_of_lines:
-            self.dictionary_final[link] = [self.d2[link], list_of_lines] #self.d2[key] greif auf die Wichtigkeit zu 
+            return [dict_full_info[link], list_of_lines] #self.dict_full_info[key] greif auf die Wichtigkeit zu 
    
-    #TODO Sontraud: Funktion testen --> d2 ist instanz aus anderer Klasse
+    #TODO Sontraud: Funktion testen --> dict_full_info ist instanz aus anderer Klasse
+    '''def paralleled(self):
+        dict_full_info = self.get_dict()
+        with futures.ThreadPoolExecutor() as ex:
+            for key in dict_full_info:
+                ex.submit(self.get_dict_of_lines, key, dict_full_info)
+        self.dictionary_final = {k: v for k, v in sorted(self.dictionary_final.items(), key=lambda item: item[1])}
+        return self.dictionary_final'''
+    
     def paralleled(self):
-        d2 = self.get_dict()
-        with futures.ProcessPoolExecutor() as ex:
-            for key in d2:
-                ex.submit(self.get_dict_of_lines, key, d2)
-            if ex.done():
-                self.dictionary_final = {k: v for k, v in sorted(self.dictionary_final.items(), key=lambda item: item[1])}
-                #return self.dictionary_final
+        dict_full_info = self.get_dict()
+        dictionary_final = {}
+        #with futures.ThreadPoolExecutor() as ex:
+        for key in dict_full_info:
+            dictionary_final[key] = self.get_dict_of_lines(key, dict_full_info)
+            #dictionary_final[key] = zwvar.result()
+        """for link in dictionary_final:
+            try:
+                print(dictionary_final[link][0])
+            except:
+                pass"""
+        """try:
+            dictionary_final = {k: v for k, v in sorted(dictionary_final.items(), key=lambda item: item[0])}
+        except:
+            print("There has been an error")
+        finally:"""
+        return dictionary_final
 
+    def print_dic(self):
+        dic = self.paralleled()
+        print(dic)
+        return
 
-
-Test1 = True
+Test1 = False
 Test2 = False
 
 if Test1:
@@ -85,10 +110,14 @@ if Test1:
     lst = t1.get_list_of_lines("Page-Rank\test.txt")
     print(lst)
 
-"""if Test2:
+if Test2:
     printout = SearchEngine("Mathe").get_list_of_lines("sorted.txt")
-    print(printout)"""
+    print(printout)
 
+search_engine = SearchEngine("KIT")
+
+
+search_engine.print_output()
 
 
 
